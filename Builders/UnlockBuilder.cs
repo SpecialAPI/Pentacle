97ab8c8b4f847f80f8c8501cf6bbad8d9d365d07
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-07-02 19:41:09 +0300

    Document UnlockBuilder

--- Builders/UnlockBuilder.cs
+++ Builders/UnlockBuilder.cs
@@ -4,8 +4,17 @@ using System.Text;
 
 namespace Pentacle.Builders
 {
+    /// <summary>
+    /// Static class that provides tools for creating custom unlocks.
+    /// </summary>
     public static class UnlockBuilder
     {
+        /// <summary>
+        /// Creates a new unlock.
+        /// </summary>
+        /// <param name="id">The string ID of the unlock.</param>
+        /// <param name="profile">Your mod profile.</param>
+        /// <returns>An object instance of the created unlock.</returns>
         public static UnlockableModData NewUnlock(string id, ModProfile profile = null)
         {
             profile ??= ProfileManager.GetProfile(Assembly.GetCallingAssembly());
@@ -15,6 +24,13 @@ namespace Pentacle.Builders
             return new UnlockableModData(profile.GetID(id));
         }
 
+        /// <summary>
+        /// Sets the custom achievement associated with the unlock.
+        /// </summary>
+        /// <typeparam name="T">The unlock's custom type. Must either be UnlockableModData or a subclass of UnlockableModData.</typeparam>
+        /// <param name="u">The object instance of the unlock.</param>
+        /// <param name="achievement">The custom achievement that will be associated with the unlock, as a ModdedAchievement_t object.</param>
+        /// <returns>The instance of the unlock, for method chaining.</returns>
         public static T SetAchievement<T>(this T u, ModdedAchievement_t achievement) where T : UnlockableModData
         {
             u.hasModdedAchievementUnlock = true;
@@ -36,6 +52,14 @@ namespace Pentacle.Builders
             return u;
         }
 
+        /// <summary>
+        /// Sets the items associated with the unlock.
+        /// </summary>
+        /// <typeparam name="T">The unlock's custom type. Must either be UnlockableModData or a subclass of UnlockableModData.</typeparam>
+        /// <param name="u">The object instance of the unlock.</param>
+        /// <param name="items">An array containing the IDs of the items that will be associated with the unlock.</param>
+        /// <param name="automaticallyLock">If true, the given items will be set as locked by default.</param>
+        /// <returns>The instance of the unlock, for method chaining.</returns>
         public static T SetItems<T>(this T u, string[] items, bool automaticallyLock = true) where T : UnlockableModData
         {
             u.hasItemUnlock = true;
@@ -70,6 +94,13 @@ namespace Pentacle.Builders
             return u;
         }
 
+        /// <summary>
+        /// Sets the quest associated with the unlock.
+        /// </summary>
+        /// <typeparam name="T">The unlock's custom type. Must either be UnlockableModData or a subclass of UnlockableModData.</typeparam>
+        /// <param name="u">The object instance of the unlock.</param>
+        /// <param name="questId">The ID of the quest that will be associated with the unlock.</param>
+        /// <returns>The instance of the unlock, for method chaining.</returns>
         public static T SetQuest<T>(this T u, string questId) where T : UnlockableModData
         {
             u.hasQuestCompletion = true;
@@ -78,6 +109,14 @@ namespace Pentacle.Builders
             return u;
         }
 
+        /// <summary>
+        /// Sets the character associated with the unlock.
+        /// </summary>
+        /// <typeparam name="T">The unlock's custom type. Must either be UnlockableModData or a subclass of UnlockableModData.</typeparam>
+        /// <param name="u">The object instance of the unlock.</param>
+        /// <param name="characterId">The ID of the character that will be associated with the unlock.</param>
+        /// <param name="automaticallyLock">If true, the given character will be set as locked by default.</param>
+        /// <returns>The instance of the unlock, for method chaining.</returns>
         public static T SetCharacter<T>(this T u, string characterId, bool automaticallyLock = true) where T : UnlockableModData
         {
             u.hasCharacterUnlock = true;
@@ -94,6 +133,13 @@ namespace Pentacle.Builders
             return u;
         }
 
+        /// <summary>
+        /// Sets custom popup associated with the unlock.
+        /// </summary>
+        /// <typeparam name="T">The unlock's custom type. Must either be UnlockableModData or a subclass of UnlockableModData.</typeparam>
+        /// <param name="u">The object instance of the unlock.</param>
+        /// <param name="customPopup">The custom unlock popup that will be associated with the unlock, as a ModUnlockInfo object.</param>
+        /// <returns>The instance of the unlock, for method chaining.</returns>
         public static T SetCustomUnlockPopup<T>(this T u, ModUnlockInfo customPopup) where T : UnlockableModData
         {
             u.modUnlockInfo = customPopup;
