e4fb470cc56b1b3746659e962af083e437dac934
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-07-16 18:55:48 +0300

    Update documentation for FogsCustomBossManager

--- CustomFogs/FogsCustomBossManager.cs
+++ CustomFogs/FogsCustomBossManager.cs
@@ -8,17 +8,17 @@ using Yarn.Unity;
 namespace Pentacle.CustomFogs
 {
     /// <summary>
-    /// A tool for adding custom final bosses to Fogs' final boss selection.
+    /// Static class that provides a tool for adding custom final bosses to Fogs' final boss selection.
     /// </summary>
     public static class FogsCustomBossManager
     {
         internal static readonly Dictionary<string, string> CustomFogsBosses = [];
 
         /// <summary>
-        /// Adds a custom final boss to Fogs' final boss selection.
+        /// Adds a custom final boss encounter as an option to Fogs' final boss selection.
         /// </summary>
-        /// <param name="bossId">The custom boss' internal bossId.</param>
-        /// <param name="dialogueOptionName">How the boss should be called in Fogs' dialogue options.</param>
+        /// <param name="bossId">The BossID of the boss encounter.<para>Not to be confused with the enemy ID of the actual boss enemy.</para></param>
+        /// <param name="dialogueOptionName">The in-game title for the boss that will be displayed in Fogs' dialogue options.</param>
         public static void AddCustomFinalBossToFogs(string bossId, string dialogueOptionName)
         {
             if (CustomFogsBosses.ContainsKey(bossId))
