b6cd7a8196920766e1c41871e5ec581bdf5b6f10
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-07-22 16:22:35 +0300

    Document CombatExtensions

--- Tools/CombatExtensions.cs
+++ Tools/CombatExtensions.cs
@@ -6,12 +6,12 @@ using System.Text;
 namespace Pentacle.Tools
 {
     /// <summary>
-    /// Combat-related extension methods.
+    /// Static class that provides extension methods for CombatManager, CombatStats and SlotsCombat.
     /// </summary>
     public static class CombatExtensions
     {
         /// <summary>
-        /// Gets all units in combat.
+        /// Returns a list of all units in combat. Charaters go before enemies, and both characters and enemies are ordered by their combat IDs.
         /// </summary>
         /// <param name="stats">Combat stats to get the units from.</param>
         /// <returns>A list of all units in combat.</returns>
@@ -26,9 +26,9 @@ namespace Pentacle.Tools
         }
 
         /// <summary>
-        /// Gets all units on field.
+        /// Gets all units on the field. Charaters go before enemies, and both characters and enemies are ordered by their combat IDs.
         /// </summary>
-        /// <param name="stats">Combat stats to get the units from</param>
+        /// <param name="stats">Combat stats to get the units from.</param>
         /// <returns>A list of all units on field.</returns>
         public static List<IUnit> UnitsOnField(this CombatStats stats)
         {
@@ -41,13 +41,13 @@ namespace Pentacle.Tools
         }
 
         /// <summary>
-        /// Tries to get a unit with a given id.
+        /// Tries to get a unit in combat.
         /// </summary>
         /// <param name="stats">The stats to get the unit from.</param>
-        /// <param name="id">The id of the unit to get.</param>
-        /// <param name="character">Is the unit a character?</param>
+        /// <param name="id">The combat ID of the unit to get.</param>
+        /// <param name="character">If true, this method will get a character. IF false, it will get an enemy.</param>
         /// <param name="unit">If successful, outputs the found unit. Otherwise, outputs null.</param>
-        /// <returns>True if successful, false otherwise.</returns>
+        /// <returns>True if the unit was successfully found, false otherwise.</returns>
         public static bool TryGetUnit(this CombatStats stats, int id, bool character, out IUnit unit)
         {
             if (character && stats.Characters.TryGetValue(id, out var cc))
@@ -69,13 +69,13 @@ namespace Pentacle.Tools
         }
 
         /// <summary>
-        /// Tries to get a unit on field with a given id.
+        /// Tries to get a unit on the field..
         /// </summary>
         /// <param name="stats">The stats to get the unit from.</param>
-        /// <param name="id">The id of the unit to get.</param>
-        /// <param name="character">Is the unit a character?</param>
+        /// <param name="id">The combat ID of the unit to get.</param>
+        /// <param name="character">If true, this method will get a character. IF false, it will get an enemy.</param>
         /// <param name="unit">If successful, outputs the found unit. Otherwise, outputs null.</param>
-        /// <returns>True if successful, false otherwise.</returns>
+        /// <returns>True if the unit was successfully found, false otherwise.</returns>
         public static bool TryGetUnitOnField(this CombatStats stats, int id, bool character, out IUnit unit)
         {
             if (character && stats.CharactersOnField.TryGetValue(id, out var cc))
@@ -96,6 +96,14 @@ namespace Pentacle.Tools
             return false;
         }
 
+        /// <summary>
+        /// Tries to get a slot in combat.
+        /// </summary>
+        /// <param name="slots">The SlotsCombat to get the slot from.</param>
+        /// <param name="slotId">The slot ID of the slot to get.</param>
+        /// <param name="isCharacterSlot">If true, this method will get a slot from the character side. If false, it will get a slot from the enemy side.</param>
+        /// <param name="slot">If successful, outputs the found slot. Otherwise, outputs null.</param>
+        /// <returns>True if the slot was successfully found, false otherwise.</returns>
         public static bool TryGetSlot(this SlotsCombat slots, int slotId, bool isCharacterSlot, out CombatSlot slot)
         {
             var list = isCharacterSlot ? slots.CharacterSlots : slots.EnemySlots;
@@ -111,6 +119,11 @@ namespace Pentacle.Tools
             return true;
         }
 
+        /// <summary>
+        /// Immediately rolls the lucky pigment chance.
+        /// </summary>
+        /// <param name="stats">Combat stats.</param>
+        /// <returns>How much lucky pigment was produced.</returns>
         public static int TriggerLuckyPigment(this CombatStats stats)
         {
             var emptySlots = stats.MainManaBar.EmptySlotsCount;
@@ -141,6 +154,11 @@ namespace Pentacle.Tools
             return amount;
         }
 
+        /// <summary>
+        /// Attempts to immedately trigger overflow.
+        /// </summary>
+        /// <param name="stats">Combat stats.</param>
+        /// <returns>True if the overflow was successfully triggered, false otherwise. If overflow is blocked (for example by using Blue Bible or Indulgence) it will be counted as unsuccessful.</returns>
         public static bool TryTriggerOverflow(this CombatStats stats)
         {
             if (stats.overflowMana.OverflowManaAmount <= 0)
