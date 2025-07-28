b3a61c264b8b87e8d325f133d90d813e50c85fc9
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-07-28 13:51:09 +0300

    Document PentaclePlugin

--- PentaclePlugin.cs
+++ PentaclePlugin.cs
@@ -7,12 +7,27 @@ using BepInEx.Logging;
 
 namespace Pentacle
 {
+    /// <summary>
+    /// Pentacle's BepInEx plugin class.
+    /// </summary>
     [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
     public class PentaclePlugin : BaseUnityPlugin
     {
+        /// <summary>
+        /// Pentacle's BepInEx plugin GUID.
+        /// </summary>
         public const string MOD_GUID = "BrutalOrchestraModding.Pentacle";
+        /// <summary>
+        /// Pentacle's BepInEx plugin name.
+        /// </summary>
         public const string MOD_NAME = "Pentacle";
+        /// <summary>
+        /// Pentacle's current version.
+        /// </summary>
         public const string MOD_VERSION = "0.0.1";
+        /// <summary>
+        /// Pentacle's mod prefix.
+        /// </summary>
         public const string MOD_PREFIX = "Pentacle";
 
         internal static Harmony HarmonyInstance;
