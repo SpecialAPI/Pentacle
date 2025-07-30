50c894355e72eec82376cc944617504d1ed030b1
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-07-30 18:07:51 +0300

    Document TargetedStatusEffectApplication

--- CustomEvent/Args/TargetedStatusEffectApplication.cs
+++ CustomEvent/Args/TargetedStatusEffectApplication.cs
@@ -4,10 +4,26 @@ using System.Text;
 
 namespace Pentacle.CustomEvent.Args
 {
+    /// <summary>
+    /// Provides information about a status effect being applied to a unit.
+    /// <para>Sent as args by CustomTriggers.StatusEffectFirstAppliedToAnyone, CustomTriggers.StatusEffectAppliedToAnyone and CustomTriggers.StatusEffectIncreasedOnAnyone</para>
+    /// </summary>
+    /// <param name="unit">The unit that the status effect was applied to.</param>
+    /// <param name="statusEffect">The applied status effect, as a StatusEffect_SO object.</param>
+    /// <param name="amountToApply">The amount of the status effect that was applied.</param>
     public class TargetedStatusEffectApplication(IUnit unit, StatusEffect_SO statusEffect, int amountToApply) : IUnitHolder
     {
+        /// <summary>
+        /// The unit that the status effect was applied to.
+        /// </summary>
         public IUnit unit = unit;
+        /// <summary>
+        /// The applied status effect, as a StatusEffect_SO object.
+        /// </summary>
         public StatusEffect_SO statusEffect = statusEffect;
+        /// <summary>
+        /// The amount of the status effect that was applied.
+        /// </summary>
         public int amountToApply = amountToApply;
 
         IUnit IUnitHolder.Value
