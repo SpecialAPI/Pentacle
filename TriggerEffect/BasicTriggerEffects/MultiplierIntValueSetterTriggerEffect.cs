2b3211bc0fa90cde4cf87974e2b0e97a98017f4e
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-08-02 22:05:51 +0300

    Document MultiplierIntValueSetterTriggerEffect

--- TriggerEffect/BasicTriggerEffects/MultiplierIntValueSetterTriggerEffect.cs
+++ TriggerEffect/BasicTriggerEffects/MultiplierIntValueSetterTriggerEffect.cs
@@ -5,10 +5,19 @@ using System.Text;
 
 namespace Pentacle.TriggerEffect.BasicTriggerEffects
 {
+    /// <summary>
+    /// A trigger effect that adds a MultiplyIntValueModifier to a value change exception stored in args.
+    /// <para>See <see cref="ValueReferenceTools.TryGetValueChangeException"/> for more information.</para>
+    /// </summary>
+    /// <param name="multiplier">The multiplier value for the MultiplyIntValueModifier that this trigger effect adds.</param>
     public class MultiplierIntValueSetterTriggerEffect(int multiplier) : TriggerEffect
     {
+        /// <summary>
+        /// The multiplier value for the MultiplyIntValueModifier that this trigger effect adds.
+        /// </summary>
         public int multiplier = multiplier;
 
+        /// <inheritdoc/>
         public override void DoEffect(IUnit sender, object args, TriggeredEffect triggerInfo, TriggerEffectExtraInfo extraInfo)
         {
             if (!ValueReferenceTools.TryGetValueChangeException(args, out var exception))
