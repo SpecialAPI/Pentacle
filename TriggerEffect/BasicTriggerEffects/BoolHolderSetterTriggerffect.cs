5608097d115f35ac54337f8563244968c6470ba0
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-08-02 17:39:15 +0300

    Document BoolHolderSetterTriggerffect

--- TriggerEffect/BasicTriggerEffects/BoolHolderSetterTriggerffect.cs
+++ TriggerEffect/BasicTriggerEffects/BoolHolderSetterTriggerffect.cs
@@ -5,11 +5,25 @@ using System.Text;
 
 namespace Pentacle.TriggerEffect.BasicTriggerEffects
 {
+    /// <summary>
+    /// A trigger effect that sets the value of a boolean holder stored in args.
+    /// <para>See <see cref="ValueReferenceTools.TryGetBoolHolder"/> for more information.</para>
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="index"></param>
     public class BoolHolderSetterTriggerffect(bool value, int index = 0) : TriggerEffect
     {
+        /// <summary>
+        /// The new value for the boolean holder.
+        /// </summary>
         public bool value = value;
+        /// <summary>
+        /// The index for the value that should be set.
+        /// <para>See <see cref="ValueReferenceTools.TryGetBoolHolder"/> for more information.</para>
+        /// </summary>
         public int index = index;
 
+        /// <inheritdoc/>
         public override void DoEffect(IUnit sender, object args, TriggeredEffect triggerInfo, TriggerEffectExtraInfo extraInfo)
         {
             if (!ValueReferenceTools.TryGetBoolHolder(args, out var boolHolder))
