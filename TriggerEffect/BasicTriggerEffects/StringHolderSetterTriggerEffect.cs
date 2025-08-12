edaaa6523bad65f1a94b028b18b5f762684560b9
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-08-12 15:26:05 +0300

    Document StringHolderSetterTriggerEffect

--- TriggerEffect/BasicTriggerEffects/StringHolderSetterTriggerEffect.cs
+++ TriggerEffect/BasicTriggerEffects/StringHolderSetterTriggerEffect.cs
@@ -5,11 +5,25 @@ using System.Text;
 
 namespace Pentacle.TriggerEffect.BasicTriggerEffects
 {
+    /// <summary>
+    /// A trigger effect that sets the value of a string holder stored in args.
+    /// <para>See <see cref="ValueReferenceTools.TryGetStringHolder"/> for more information.</para>
+    /// </summary>
+    /// <param name="value">The new value for the string holder.</param>
+    /// <param name="index">The index for the value that should be set.<para>See <see cref="ValueReferenceTools.TryGetStringHolder"/> for more information.</para></param>
     public class StringHolderSetterTriggerEffect(string value, int index = 0) : TriggerEffect
     {
+        /// <summary>
+        /// The new value for the string holder.
+        /// </summary>
         public string value = value;
+        /// <summary>
+        /// The index for the value that should be set.
+        /// <para>See <see cref="ValueReferenceTools.TryGetStringHolder"/> for more information.</para>
+        /// </summary>
         public int index = index;
 
+        /// <inheritdoc/>
         public override void DoEffect(IUnit sender, object args, TriggeredEffect triggerInfo, TriggerEffectExtraInfo extraInfo)
         {
             if (!ValueReferenceTools.TryGetStringHolder(args, out var stringRef))
