65dbc86a63f5c8e703c558f776d4f44a5a92df36
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-08-16 20:30:51 +0300

    Document FakeDamageEffect

--- Effects/FakeDamageEffect.cs
+++ Effects/FakeDamageEffect.cs
@@ -5,10 +5,18 @@ using System.Text;
 
 namespace Pentacle.Effects
 {
+    /// <summary>
+    /// An effect that deals fake damage to all targets equal to entryVariable.
+    /// <para>See <see cref="IUnitExtensions.FakeDamage"/> for more information.</para>
+    /// </summary>
     public class FakeDamageEffect : EffectSO
     {
+        /// <summary>
+        /// Determines if the fake damage should be direct or indirect.
+        /// </summary>
         public bool directDamage = true;
 
+        /// <inheritdoc/>
         public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
         {
             exitAmount = 0;
