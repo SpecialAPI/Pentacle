108da82a2faa67845d19e0367a78deb90b79d2e4
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2025-08-26 16:36:03 +0300

    Fix broken documentation for ScriptableObjectTools.CreateScriptable

--- Tools/ScriptableObjectTools.cs
+++ Tools/ScriptableObjectTools.cs
@@ -13,7 +13,7 @@ namespace Pentacle.Tools
         /// Creates a scriptable object of a certain type. This method can be given an action that will be immediately invoked on the created scriptable object, which can be used to immediately set the scriptable object's values.
         /// </summary>
         /// <typeparam name="T">The type for the created object. Must be a subclass of ScriptableObject.</typeparam>
-        /// <param name="configure">An action that immediately gets invoked on the created scriptable object. This can be used to immediately set the scriptable object's values, similarly to how constructors work for regular objects./param>
+        /// <param name="configure">An action that immediately gets invoked on the created scriptable object. This can be used to immediately set the scriptable object's values, similarly to how constructors work for regular objects.</param>
         /// <returns>The created scriptable object.</returns>
         public static T CreateScriptable<T>(Action<T> configure = null) where T : ScriptableObject
         {
