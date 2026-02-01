2b1b66847abc13065f89889e369115a690639b84
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-02-01 23:13:06 +0300

    Update documentation in ValueReferenceTools

--- Tools/ValueReferenceTools.cs
+++ Tools/ValueReferenceTools.cs
@@ -16,6 +16,8 @@ namespace Pentacle.Tools
         /// <list type="bullet">
         /// <item>If <paramref name="args"/> is already a subclass of IBoolHolder, outputs <paramref name="args"/>.</item>
         /// <item>If <paramref name="args"/> is a BooleanReference, outputs an IBoolHolder whose value is connected to the BooleanReference's value.</item>
+        /// <item>If <paramref name="args"/> is a IntegerReference_Damage, whose 2 read-only values get the IntegerReference_Damage's directDamage and ignoresShield respectively.</item>
+        /// <item>If <paramref name="args"/> is a IntegerReference_Heal, whose read-only value gets the IntegerReference_Heal's directHeal.</item>
         /// <item>If <paramref name="args"/> is a BooleanWithTriggerReference, outputs an IBoolHolder whose value is connected to the BooleanWithTriggerReference's value.</item>
         /// <item>If <paramref name="args"/> is a StatusFieldApplication, outputs an IBoolHolder whose primary value is connected to the StatusFieldApplication's canBeApplied and whose secondary read-only value gets the StatusFieldApplication's isStatusPositive.</item>
         /// <item>If <paramref name="args"/> is a DamageReceivedValueChangeException, outputs an IBoolHolder whose 2 read-only values get the DamageReceivedValueChangeException's directDamage and ignoresShield respectively.</item>
@@ -49,6 +51,8 @@ namespace Pentacle.Tools
         /// Tries to convert <paramref name="args"/> to an IIntHolder:
         /// <list type="bullet">
         /// <item>If <paramref name="args"/> is already a subclass of IIntHolder, outputs <paramref name="args"/>.</item>
+        /// <item>If <paramref name="args"/> is a IntegerReference_Damage, outputs an IIntHolder whose first value is connected to the IntegerReference_Damage's value, and whose second and third values are read-only and get the IntegerReference_Damage's affectedStartSlot and affectedEndSlot respectively.</item>
+        /// <item>If <paramref name="args"/> is a IntegerReference_Heal, outputs an IIntHolder whose value is connected to the IntegerReference_Heal's value.</item>
         /// <item>If <paramref name="args"/> is a IntegerReference, outputs an IIntHolder whose value is connected to the IntegerReference's value.</item>
         /// <item>If <paramref name="args"/> is a StatusFieldApplication, outputs an IIntHolder whose value is connected to the StatusFieldApplication's amount.</item>
         /// </list>
@@ -79,6 +83,8 @@ namespace Pentacle.Tools
         /// <list type="bullet">
         /// <item>If <paramref name="args"/> is already a subclass of IStringHolder, outputs <paramref name="args"/>.</item>
         /// <item>If <paramref name="args"/> is a StringReference, outputs an IStringHolder whose value is connected to the StringReference's value.</item>
+        /// <item>If <paramref name="args"/> is a IntegerReference_Damage, outputs an IStringHolder whose 2 read-only values get the IntegerReference_Damage's damageTypeID and deathTypeID respectively.</item>
+        /// <item>If <paramref name="args"/> is a IntegerReference_Heal, outputs an IStringHolder whose read-only value gets the IntegerReference_Heal's healTypeID.</item>
         /// <item>If <paramref name="args"/> is a StatusFieldApplication, outputs an IStringHolder whose read-only value gets the StatusFieldApplication's statusID.</item>
         /// <item>If <paramref name="args"/> is a DamageReceivedValueChangeException, outputs an IStringHolder whose read-only value gets the DamageReceivedValueChangeExceptionHolder's damageTypeID.</item>
         /// </list>
@@ -108,10 +114,13 @@ namespace Pentacle.Tools
         /// Tries to convert <paramref name="args"/> to an IUnitHolder:
         /// <list type="bullet">
         /// <item>If <paramref name="args"/> is already a subclass of IUnitHolder, outputs <paramref name="args"/>.</item>
+        /// <item>If <paramref name="args"/> is a IntegerReference_Damage, outputs an IUnitHolder whose 2 read-only values get the IntegerReference_Damage's possibleSourceUnit and damagedUnit respectively.</item>
+        /// <item>If <paramref name="args"/> is a IntegerReference_Heal, outputs an IUnitHolder whose 2 read-only values get the IntegerReference_Heal's possibleSourceUnit and healedUnit respectively.</item>
         /// <item>If <paramref name="args"/> is a DamageDealtValueChangeException, outputs an IUnitHolder whose 2 read-only values get the DamageDealtValueChangeException's damagedUnit and casterUnit respectively.</item>
         /// <item>If <paramref name="args"/> is a DamageReceivedValueChangeException, outputs an IUnitHolder whose 2 read-only values get the DamageReceivedValueChangeException's possibleSourceUnit and damagedUnit respectively.</item>
         /// <item>If <paramref name="args"/> is a HealingDealtValueChangeException, outputs an IUnitHolder whose 2 read-only values get the HealingDealtValueChangeException's healingUnit and casterUnit respectively.</item>
         /// <item>If <paramref name="args"/> is a HealingReceivedValueChangeException, outputs an IUnitHolder whose 2 read-only values get the HealingReceivedValueChangeException's possibleSourceUnit and healingUnit respectively.</item>
+        /// <item>If <paramref name="args"/> is a IUnit, outputs an IUnitHolder whose read-only value gets that IUnit.</item>
         /// </list>
         /// </summary>
         /// <param name="args">The object to convert to an IUnitHolder.</param>
