590727ec168668d91a1f8ec6a7e3deb5f1cf7867
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-02-01 22:09:12 +0300

    Fix SimpleUnitHolder's properties using the unit parameter instead of the field

--- Triggers/Args/BasegameReferenceHolders/SimpleUnitHolder.cs
+++ Triggers/Args/BasegameReferenceHolders/SimpleUnitHolder.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace Pentacle.Triggers.Args.BasegameReferenceHolders
 {
-    internal class SimpleUnitHolder(IUnit u) : IUnitHolder
+    internal class SimpleUnitHolder(IUnit unit) : IUnitHolder
     {
-        public IUnit unit = u;
+        public IUnit unit = unit;
 
-        public IUnit this[int index] { get => u; set => PentacleLogger.LogWarning($"SimpleUnitHolder's Unit value is read-only."); }
-        public IUnit Value { get => u; set => PentacleLogger.LogWarning($"SimpleUnitHolder's Unit value is read-only."); }
+        public IUnit this[int index] { get => unit; set => PentacleLogger.LogWarning($"SimpleUnitHolder's Unit value is read-only."); }
+        public IUnit Value { get => unit; set => PentacleLogger.LogWarning($"SimpleUnitHolder's Unit value is read-only."); }
     }
 }
