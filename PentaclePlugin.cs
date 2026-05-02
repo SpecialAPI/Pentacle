73bd86f78de67cc6a25efb8e40d68cfb4d5debcf
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-05-02 16:30:09 +0300

    Bump plugin version

--- PentaclePlugin.cs
+++ PentaclePlugin.cs
@@ -24,7 +24,7 @@ namespace Pentacle
         /// <summary>
         /// Pentacle's current version.
         /// </summary>
-        public const string MOD_VERSION = "0.0.1";
+        public const string MOD_VERSION = "0.0.2";
         /// <summary>
         /// Pentacle's mod prefix.
         /// </summary>
